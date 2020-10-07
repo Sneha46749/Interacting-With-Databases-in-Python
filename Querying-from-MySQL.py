24d10286260ef516d742cd8e3276fe934432e8bd
Sneha <sneha46749@gmail.com>
2020-10-07 22:59:19 +0530

    Querying data from a MySQL database via Python

--- Querying-from-MySQL.py
+++ Querying-from-MySQL.py
@@ -6,6 +6,7 @@ con = mysql.connector.connect(
     host="108.167.140.122", 
     database = "ardit700_pm1database"
 )
+
 cursor = con.cursor()
 query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
 results = cursor.fetchall()
